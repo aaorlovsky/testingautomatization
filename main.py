423c3f82fb41245e9eddd8551b76a80cf4cd600e
Alexander Orlovsky <aaorlovsky@hotmail.com>
2023-02-20 19:11:47 +0300

    prevent browser before close

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-print('Hello')
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+# driver.close()
+
